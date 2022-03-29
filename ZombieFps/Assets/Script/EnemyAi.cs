78379cb63e68bed696bb9dcd8584a488b92b8398
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-29 09:25:37 +0200

    Added standard library and small Enemy AI

---
+++ ZombieFps/Assets/Script/EnemyAi.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaserRange = 5f;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaserRange) { 
+            isProvoked = true;
+        } 
+       
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+        
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has seek and is destroying");
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaserRange);
+    }
+}
