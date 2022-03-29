b1b6cdbe5dc35849341f69048d6885e9c4020f57
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-29 10:36:13 +0200

    Imported gun and added reticle + raycasting

---
+++ ZombieFps/Assets/Script/EnemyHealth.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitpoints = 100f;
+
+}
---
+++ ZombieFps/Assets/Script/Weapon.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(FPCamera.transform.position,FPCamera.transform.forward,out hit,range) {
+            Debug.Log("I hit this thing:" + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+        } else
+        {
+            return;
+        }
+    }
+}
